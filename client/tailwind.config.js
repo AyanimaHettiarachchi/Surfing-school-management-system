aa0b2f2c95a1ef1d5d336c02d2f12cfc1162fc95
AyanimaHettiarachchi <172464573+AyanimaHettiarachchi@users.noreply.github.com>
2024-08-19 08:32:56 +0530

    install react js and tailwind css and create the first template

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',  // Add the correct paths
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
